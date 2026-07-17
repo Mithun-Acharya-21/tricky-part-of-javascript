06badda36fc6ef1da86cb2e030bc65d65645e93e
Mithun H <mh1295551@gmail.com>
2026-07-17 13:20:04 +0530

    feat: add event loop visualization

--- hoisting.js
+++ hoisting.js
@@ -8,3 +8,9 @@ function squareNumber(num) {
   return num **2;
 } // console.log(squareNumber(4)); // 16 //for function declarations, hoisting works
 
+
+// feat: add event loop visualization
+// Commit #4 | 2026-07-24 22:57:00
+function addeventloopvisualization() {
+  return true;
+}
